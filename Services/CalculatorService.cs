9485cc7e8ac3f5af9be52864eada9d2a718f7bb1
Ooperalta <164176557+Ooperalta@users.noreply.github.com>
2026-08-22 13:53:28 +0100

    feat: implement calculator service with basic operations

---
+++ Services/CalculatorService.cs
@@ -0,0 +1,16 @@
+namespace TerminalCalculator.Services;
+
+public class Calculator : ICalculatorService
+{
+    public double Add(double num1, double num2)
+    {
+        return num1 + num2;
+    }
+
+    public double Subtract(double num1, double num2)
+    {
+        return num1 - num2;
+    }
+        
+    
+}
