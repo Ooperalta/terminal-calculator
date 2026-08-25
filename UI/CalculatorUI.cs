5fc5e9b1b8f93dc8e625cdb391197a0fa209da5e
Ooperalta <164176557+Ooperalta@users.noreply.github.com>
2026-08-25 13:40:03 +0100

    feat: enhance CalculatorUI with complete operation logic and error handling

--- UI/CalculatorUI.cs
+++ UI/CalculatorUI.cs
@@ -1,22 +1,25 @@
 using TerminalCalculator.Services;
 
 namespace TerminalCalculator.UI;
+
 public class CalculatorUI
 {
-private readonly ICalculatorService _calculatorService;
+    private readonly ICalculatorService _calculatorService;
 
-public CalculatorUI(ICalculatorService calculatorService)
-{
-    _calculatorService = calculatorService;
-}
-
-public void Run()
+    public CalculatorUI(ICalculatorService calculatorService)
     {
-        while(true)
+        _calculatorService = calculatorService;
+    }
+
+
+
+    public void Run()
+    {
+        while (true)
         {
             Console.Clear();
-        
-               Console.WriteLine("""
+
+            Console.WriteLine("""
             ===========================
                TERMINAL CALCULATOR
             ===========================
@@ -31,30 +34,87 @@ public void Run()
 
             string? choice = Console.ReadLine();
 
+            double num1;
+            double num2;
+            double result;
+            string askFirstNumber = "Enter the first number: ";
+            string askSecondNumber = "Enter the second number: ";
+
             switch (choice)
             {
                 case "1":
-                // Add Logic
-                break;
+                    num1 = GetNumberFromUser(askFirstNumber);
+                    num2 = GetNumberFromUser(askSecondNumber);
+                    result = _calculatorService.Add(num1, num2);
+
+                    Console.WriteLine($"Result: {result}");
+                    Console.ReadLine();
+                    break;
                 case "2":
-                // Subtract Logic
-                break;
+                    num1 = GetNumberFromUser(askFirstNumber);
+                    num2 = GetNumberFromUser(askSecondNumber);
+                    result = _calculatorService.Subtract(num1, num2);
+
+                    Console.WriteLine($"Result: {result}");
+                    Console.ReadLine();
+                    break;
                 case "3":
-                // Multiply Logic
-                break;
+                    num1 = GetNumberFromUser(askFirstNumber);
+                    num2 = GetNumberFromUser(askSecondNumber);
+                    result = _calculatorService.Multiply(num1, num2);
+
+                    Console.WriteLine($"Result: {result}");
+                    Console.ReadLine();
+                    break;
                 case "4":
-                // Divide Logic
-                break;
+                    try
+                    {
+                        num1 = GetNumberFromUser(askFirstNumber);
+                        num2 = GetNumberFromUser(askSecondNumber);
+                        result = _calculatorService.Divide(num1, num2);
+
+                        Console.WriteLine($"Result: {result}");
+                        Console.ReadLine();
+                    }
+                    catch (DivideByZeroException ex)
+                    {
+                        Console.WriteLine(ex.Message);
+                        Console.ReadLine();
+                    }
+
+                    break;
                 case "0":
-                Console.WriteLine("Exiting the Calculator. Bye!");
-                return;
+                    Console.WriteLine("Exiting the Calculator. Bye!");
+                    return;
                 default:
-                Console.WriteLine("Invalid option. Please try again.");
-                break;
+                    Console.WriteLine("Invalid option. Please try again.");
+                    Console.ReadLine();
+                    break;
             }
-       
+
         }
-        
+
 
     }
+
+    private double GetNumberFromUser(string message)
+    {
+        while (true)
+        {
+            Console.Write(message);
+            string? input = Console.ReadLine();
+            if (double.TryParse(input, out double convertedNumber))
+            {
+                return convertedNumber;
+            }
+            else
+            {
+                Console.WriteLine("Invalid number. Please enter a valid number.");
+            }
+        }
+
+
+    }
+
+
 }
